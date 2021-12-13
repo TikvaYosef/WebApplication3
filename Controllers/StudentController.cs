2c14851295af0678f6d89a06b0f53fcb0c977f11
TikvaYosef <tikvayosef13@gmail.com>
2021-12-13 19:31:44 +0200

    commit

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -21,14 +21,14 @@ namespace WebApplication3.Controllers
         }
         public ActionResult ShowStudent()
         {
-            
+
             return View();
         }
         public ActionResult ShowNamesOfStudents()
         {
             string[] namesOfStudent = new string[] { "tikva", "keren", "yafit", "eden", "lior", "aschalo" };
             ViewBag.array = namesOfStudent;
-            return View() ;
+            return View();
         }
         public ActionResult ShowStudents()
         {
@@ -36,38 +36,46 @@ namespace WebApplication3.Controllers
         }
         public ActionResult JsonObject()
         {
-            string[] array4= new string[] { "tikva" ,"keren","25" };
-           
-            return Json(array4,JsonRequestBehavior.AllowGet);
+            string[] array4 = new string[] { "tikva", "keren", "25" };
+
+            return Json(array4, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult ObjectS()
         {
-            Student student1 = new Student("tikva","yosef",1996,12);
+            Student student1 = new Student("tikva", "yosef", 1996, 12);
             ViewBag.student = student1;
             return View();
         }
         public ActionResult ObjectS2()
         {
-            Student student1 = new Student("keren", "hillu", 1998, 12);
-            return View(student1);
+            return View(Ob1());
+        }
+        static Student Ob1()
+        {
+            Student student1 = new Student("keren", "hailu", 1998, 12);
+            return  student1;
+
+
         }
 
         public ActionResult Objects4()
         {
-            Student student2 = new Student("shay","balayna",2000,10);
+            return View(Ob());
+        }
+
+        static List<Student> Ob()
+        {
+            Student student2 = new Student("shay", "balayna", 2000, 10);
             Student student3 = new Student("keren", "haillu", 1998, 13);
-            Student student4 = new Student("eden","testma",1996,12);
+            Student student4 = new Student("eden", "testma", 1996, 12);
             Student student5 = new Student("yafit", "samuel", 1997, 11);
             List<Student> list1 = new List<Student>();
             list1.Add(student2);
             list1.Add(student3);
             list1.Add(student4);
             list1.Add(student5);
-
-
-
-            return View(list1);
+            return list1;
         }
 
     }
